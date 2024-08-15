7d5282868097a5d3b2f6cd9dcbf0658b12938493
Abhishek Mishra <166637489+mrabhishekmishra@users.noreply.github.com>
2024-08-15 17:10:28 +0530

    lawn prep project start and add

---
+++ js/js.js
@@ -0,0 +1,12 @@
+// top slider js
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1, 
+  // items change number for slider display on desktop
+  
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
